fa008a6cf11ea38af73c1752644e76f03d3616aa
hiluey <giovannahilueyd@gmail.com>
2025-04-29 22:37:47 +0000

    dark detect

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', 
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-
-theme: {
-  extend: {
-    animation: {
-      fadeIn: 'fadeIn 0.2s ease-in-out',
-    },
-    keyframes: {
-      fadeIn: {
-        '0%': { opacity: 0, transform: 'translateY(-5px)' },
-        '100%': { opacity: 1, transform: 'translateY(0)' },
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.2s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0, transform: 'translateY(-5px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
       },
     },
   },
-},
+  plugins: [],
 }
